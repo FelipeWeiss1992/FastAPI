ffab2b956a302711ca66da9774b1108a84354357
Felipe Weiss <felipe.weiss@proway.treina>
2023-01-27 22:03:30 -0300

    FastAPI

--- FSTAPI/main.py
+++ FSTAPI/main.py
@@ -1,29 +1,46 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException, status
+from models import Aluno
 app = FastAPI()
 
-@app.get('/')
-async def home():
-    return{
-        "message": "Hello World"
-    }
 
 alunos = {
 
-    1 : "Felipe",
-    2 : "Jean",
-    3 : "Dieter",
-    4 : "David"
+    1 : {'nome' : "Felipe",'idade' : 30, 'email' : 'felipeweiss92@gmail.com'},
+    2 : {'nome' : "Jean",'idade' : 40, 'email' : 'jean@gmail.com'},
+    3 : {'nome' : "Dieter",'idade' : 40, 'email' :'dieter@gmail.com'},
+    4 : {'nome' : "David", 'idade' : 35, 'email' : 'david@gmail.com'}
 
 }
 
 @app.get('/alunos')
-async def get_alunos():
+async def get_AlunosID():
     return alunos
 
-@app.get('/alunos/{item_id}')
-def get_AlunosID(item_id: int):
-    return alunos[item_id]
+@app.get('/alunos/{aluno_id}')
+async def get_AlunoID(aluno_id: int):
+    try:
+        aluno = alunos[aluno_id]
+        alunos.update({'id': aluno_id})
+        return aluno
+    except KeyError:
+        raise HTTPException(
+
+        status_code = status.HTTP_404_NOT_FOUND, detail='Aluno não encontrado'
+
+        )
+        
+@app.get('/alunosnome/{aluno_nome}')
+async def get_Nomealuno(aluno_nome: str):
+    for aluno in alunos.values():
+        if aluno['nome'] == aluno_nome:
+            return aluno
+        
+@app.post('/alunonovo', status_code=status.HTTP_201_CREATED)
+async def post_NovoAluno(aluno_novo: Aluno):
+    next_id : int = len(alunos) + 1
+    alunos[next_id] = aluno_novo
+    return aluno_novo
+    
 
 
 if __name__ == '__main__':
@@ -36,3 +53,4 @@ if __name__ == '__main__':
         log_level= 'info',
         reload = True
     )
+
